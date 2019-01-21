8bfa3aafb7570c1f378af207afc16be50c272d41
William Myers <williamrmyers@gmail.com>
2019-01-21 00:11:40 -0800

    Fixed overflow issue.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Bar from './SideBar/Bar';
-import SongList from './SongList/SongList';
-import './App.css';
+import React from "react";
+import Bar from "./SideBar/Bar";
+import SongList from "./SongList/SongList";
+import "./App.css";
 
 const App = () => {
-  return(
-    <div className="ui equal width grid app">
-      <Bar />
-      <SongList />
+  return (
+    <div className="app">
+      <div className="ui equal width grid">
+        <Bar />
+        <SongList />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
